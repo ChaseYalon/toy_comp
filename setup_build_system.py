80cb3f08e35df3cda978f8c028825afb2b4421d4
Chase Yalon <Chaseyalon@gmail.com>
2025-12-18 15:25:52 +0000

    stupid mistake

--- setup_build_system.py
+++ setup_build_system.py
@@ -166,7 +166,7 @@ if os_name == "Windows":
         ["ls", "-la", MINGW64_BIN],
         check=True
     )
-    subprocess.run(f"#./{MINGW64_BIN}/llvm-config.exe", shell=True)
+    subprocess.run(f"./{MINGW64_BIN}/llvm-config.exe", shell=True)
     env["LIBCLANG_PATH"] = MINGW64_BIN
 
     subprocess.run(
